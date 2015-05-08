3522c31c9868cb8e894a90849e98071ea6a4338f
Orgil <orgil.u@gmail.com>
2015-05-08 08:37:05 +0800

    added magit instruction seen notes

--- config/setup-magit.el
+++ config/setup-magit.el
@@ -1,3 +1,5 @@
 (require 'magit)
 
+(setq magit-last-seen-setup-instructions "1.4.0")
+
 (provide 'setup-magit)
--- init.el
+++ init.el
@@ -75,7 +75,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+ '(neo-theme (quote nerd)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
