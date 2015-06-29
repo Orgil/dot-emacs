e5fcbf33d46c495e24065c2753f4ead1fbbcde37
Orgil <orgil.u@gmail.com>
2015-06-29 16:00:44 +0800

    adding jscs checker config to flycheck

--- config/setup-flycheck.el
+++ config/setup-flycheck.el
@@ -18,4 +18,24 @@
 (setq flycheck-clang-include-path
                            (list (expand-file-name "/usr/local/include/")))
 
+;; Flycheck JSCS
+(flycheck-def-config-file-var flycheck-jscs javascript-jscs ".jscs.json"
+                              :safe #'stringp)
+
+(flycheck-define-checker javascript-jscs
+                         "A JavaScript code style checker.
+                         See URL `https://github.com/mdevils/node-jscs'."
+                         :command ("jscs" "--reporter" "checkstyle"
+                                   (config-file "--config" flycheck-jscs)
+                                   source)
+                         :error-parser flycheck-parse-checkstyle
+                         :modes (js-mode js2-mode js3-mode)
+                         :next-checkers (javascript-jshint))
+
+(defun jscs-enable () (interactive)
+  (add-to-list 'flycheck-checkers 'javascript-jscs))
+
+(defun jscs-disable () (interactive)
+  (setq flycheck-checkers (remove 'javascript-jscs flycheck-checkers)))
+
 (provide 'setup-flycheck)
